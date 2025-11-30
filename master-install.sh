6e51c6242724d3ff555e1f41b683c2178f61b426
LandMineDevelopment <kccandelario@gmail.com>
2025-11-30 01:10:42 -0500

    Correct alias check for yazi in bashrc
    Fix the grep command to check for alias in the correct file path.

--- master-install.sh
+++ master-install.sh
@@ -13,7 +13,7 @@ if [ ! -f ~/.bashrc ]; then
     exit 1
 fi
 # check if yazi alias exists
-if grep -Fxq "alias y='yazi'"; then
+if grep -Fxq "alias y='yazi'" ~.bashrc; then
     echo "yazi alias already exists"
 else
     echo "setting alias y='yazi'"
